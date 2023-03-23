ee05e01d751e5c7ce0965e0f7c309a5035ffbc59
Chinedu-jo <macjoe55@gmail.com>
2023-03-23 22:08:30 +0000

    app logger and

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -72,8 +73,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -143,6 +144,8 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  app.logger.info("AUTH HEADER") #added this line
+  app.logger.info(request.headers.get('Authorization')) #added this line
   data = HomeActivities.run()
   return data, 200
 
